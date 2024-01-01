5da4cb75d47e0fb0b175cd16bec779809d762c35
IvanDanilovFD03 <ivd@empat.tech>
2024-01-01 18:14:05 +0200

    feat: create greedy model

---
+++ models/random/greedy_model.py
@@ -0,0 +1,164 @@
+import random
+import numpy as np
+
+from models.model import Model
+
+
+class Greedy(Model):
+    def __init__(self, num_samples, n_rows, n_cols, n_colors, score, combo):
+        self.NUMSAMPLES = num_samples
+        self.NROWS = n_rows
+        self.NCOLS = n_cols
+        self.NCOLORS = n_colors
+        self.SCORE = score
+        self.COMBO = combo
+
+    def drop_columns(self, game_field, colsToRowsMap):
+        for col in colsToRowsMap:
+            currRows = colsToRowsMap[col]
+            minRow = currRows[0]
+            maxRow = currRows[1]
+            diff = maxRow - minRow + 1
+            for i in range(minRow):
+                game_field[minRow - 1 - i + diff,
+                           col] = game_field[minRow - 1 - i, col]
+            for i in range(diff):
+                game_field[i, col] = random.randint(1, self.NCOLORS)
+
+    def explore_coord(self, game_field, i, j):
+        color = game_field[i, j]
+        colorRowsSet = set([(i, j)])
+        colorColsSet = set([(i, j)])
+        for row in range(i):
+            if color != game_field[i - row - 1, j]:
+                break
+            colorRowsSet.add((i - row - 1, j))
+        for row in range(i + 1, self.NROWS):
+            if color != game_field[row, j]:
+                break
+            colorRowsSet.add((row, j))
+        for col in range(j):
+            if color != game_field[i, j - col - 1]:
+                break
+            colorColsSet.add((i, j - col - 1))
+        for col in range(j + 1, self.NCOLS):
+            if color != game_field[i, col]:
+                break
+            colorColsSet.add((i, col))
+        return colorRowsSet, colorColsSet
+
+    def cascade_coords(self, game_field):
+        coords = set()
+        for i in range(self.NROWS):
+            for j in range(self.NCOLS):
+                colorRowsSet, colorColsSet = self.explore_coord(
+                    game_field, i, j)
+                colorAllSet = set()
+                if len(colorRowsSet) >= 3:
+                    colorAllSet = colorRowsSet
+                if len(colorColsSet) >= 3:
+                    colorAllSet = colorAllSet.union(colorColsSet)
+                if len(colorAllSet) > len(coords):
+                    coords = colorAllSet
+        return coords
+
+    def cascade(self, game_field):
+        score = 0
+        combo = 1
+        while True:
+            coords = self.cascade_coords(game_field)
+            cascadeSize = len(coords)
+            if cascadeSize < 3:
+                break
+            add_score = 10 * (cascadeSize ** 2 - cascadeSize) * combo
+            score += add_score
+            combo += 1
+            colsToRowsMap = {}
+            for coord in coords:
+                if coord[1] not in colsToRowsMap:
+                    colsToRowsMap[coord[1]] = [coord[0], coord[0]]
+                else:
+                    currRows = colsToRowsMap[coord[1]]
+                    currRowMin = currRows[0]
+                    currRowMax = currRows[1]
+                    if coord[0] < currRowMin:
+                        currRows[0] = coord[0]
+                    elif coord[0] > currRowMax:
+                        currRows[1] = coord[0]
+            self.drop_columns(game_field, colsToRowsMap)
+
+        return score, combo
+
+    def check_coord_validity(self, coord):
+        if (coord[0] < 0 or coord[0] >= self.NROWS) or (coord[1] < 0 or coord[1] >= self.NCOLS):
+            return False
+        else:
+            return True
+
+    def check_move_validity(self, game_field, coord1, coord2):
+        if self.check_coord_validity(coord1) and self.check_coord_validity(coord2):
+            if (abs(coord1[0] - coord2[0]) == 1 and coord1[1] == coord2[1]) or (
+                    abs(coord1[1] - coord2[1]) == 1 and coord1[0] == coord2[0]):
+                game_field_copy = np.copy(game_field)
+                game_field_copy[coord1], game_field_copy[coord2] = game_field_copy[coord2], game_field_copy[coord1]
+                colorRowsSet1, colorColsSet1 = self.explore_coord(
+                    game_field_copy, coord1[0], coord1[1])
+                if len(colorRowsSet1) >= 3 or len(colorColsSet1) >= 3:
+                    return True
+                colorRowsSet2, colorColsSet2 = self.explore_coord(
+                    game_field_copy, coord2[0], coord2[1])
+                if len(colorRowsSet2) >= 3 or len(colorColsSet2) >= 3:
+                    return True
+        return False
+
+    def find_optimal_movement(self, game_field):
+        self.SCORE = 0
+        self.COMBO = 1
+        coord1 = []
+        coord2 = []
+        max_score = 0
+        for i in range(self.NROWS):
+            for j in range(self.NCOLS):
+                temp_game_field = np.array(game_field)
+                if self.check_move_validity(game_field, (i, j), (i, j + 1)):
+                    candidate_coord1 = (i, j)
+                    candidate_coord2 = (i, j + 1)
+                    temp_game_field[candidate_coord1], temp_game_field[candidate_coord2] = temp_game_field[
+                        candidate_coord2], temp_game_field[candidate_coord1]
+                    turn_score, self.COMBO = self.cascade(temp_game_field)
+                    if turn_score > max_score:
+                        max_score = turn_score
+                        coord1 = candidate_coord1
+                        coord2 = candidate_coord2
+                elif self.check_move_validity(game_field, (i, j), (i + 1, j)):
+                    candidate_coord1 = (i, j)
+                    candidate_coord2 = (i + 1, j)
+                    temp_game_field[candidate_coord1], temp_game_field[candidate_coord2] = temp_game_field[
+                        candidate_coord2], temp_game_field[candidate_coord1]
+                    turn_score, self.COMBO = self.cascade(temp_game_field)
+                    if turn_score > max_score:
+                        max_score = turn_score
+                        coord1 = candidate_coord1
+                        coord2 = candidate_coord2
+        self.SCORE, self.COMBO = self.cascade(game_field)
+        self.SCORE += self.SCORE
+
+        return [coord1, coord2]
+
+
+# greedy = Greedy(1000, 9, 9, 5, 0, 1)
+
+# game_field = [[2, 5, 5, 2, 4, 3, 5, 2, 4],
+#         [3, 2, 2, 1, 5, 1, 5, 5, 1],
+#         [2, 2, 4, 4, 1, 3, 1, 5, 2],
+#         [5, 1, 5, 2, 5, 1, 4, 3, 4],
+#         [1, 3, 4, 5, 1, 2, 3, 3, 5],
+#         [1, 4, 1, 5, 3, 3, 4, 5, 4],
+#         [3, 5, 2, 1, 1, 4, 3, 1, 4],
+#         [2, 3, 4, 3, 1, 2, 5, 3, 5],
+#         [4, 2, 4, 2, 5, 3, 2, 3, 4]]
+
+# game_field = np.array(game_field)
+
+# action = greedy.find_optimal_movement(game_field)
+# print(action)
